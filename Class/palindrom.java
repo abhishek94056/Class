5cf15d65ca870341815d7d06b7c92cafb7a00b77
uSER <uSER@DESKTOP-C3HV41Q>
2025-04-17 00:03:40 +0530

    commit initial.

---
+++ Class/palindrom.java
@@ -0,0 +1,20 @@
+
+public class palindrom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int num = 1;num<=100;num++)
+		{
+			if(num%4==0)
+			{
+				System.out.println(num+" is even and divisible by 4");
+			}
+			
+		}
+		
+		
+
+	}
+
+}
